b5a1c9e3da80157e92ed8ee81a4a76b11de1db6d
chmadran <charlotte.madrangeas@gmail.com>
2023-12-12 10:48:54 +0100

    Clean Auth Module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
