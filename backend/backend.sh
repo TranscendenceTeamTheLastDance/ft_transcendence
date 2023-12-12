7b7b5aa62219be3e49a4d580600b623998ac52e1
nicolasbene <nibenoit@student.42.fr>
2023-12-12 16:32:25 +0100

    backend and postgres work with docker + prisma migration et studio

--- backend/backend.sh
+++ backend/backend.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 
-export DATABASE_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:${POSTGRES_PORT}/${POSTGRES_DB}?schema=public"
-
 npm install
 npx prisma generate
 
 echo "-----Migrating database-----"
 
-while true; do
-	echo "-----Migrating database try-----"
-	npx prisma migrate dev --name init
-	EXIT_CODE=$?
-	echo "PRISMA EXIT CODE: $EXIT_CODE"
-	if [ $EXIT_CODE -eq 0 ]; then
-		break
-	fi
-done
+npx prisma migrate dev --name init --preview-feature
+
+echo "-----Starting Prisma Studio on port 5555-----"
+
+npx prisma studio --port 5555 &
 
 echo "-----Starting backend-----"
 
