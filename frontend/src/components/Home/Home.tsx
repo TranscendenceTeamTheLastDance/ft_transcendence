36c5ee92eaf2bfe503d0770ef03338d003ec72b5
Charlotte MADRANGEAS <charlotte.madrangeas@gmail.com>
2023-12-18 18:46:42 +0100

    Add home page etc

---
+++ frontend/src/components/Home/Home.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Particles from './Particles.tsx';
+
+const navigation = [
+  { name: "CHAT", href: "/chat" },
+  { name: "PLAY", href: "/play" },
+];
+
+const Home = () => {
+  return (
+	<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+	  <nav className="my-16 animate-fade-in">
+		<ul className="flex items-center justify-center gap-4">
+		  {navigation.map((item) => (
+			<li key={item.href}>
+			  <Link
+				to={item.href}
+				className="text-2xl duration-500 text-zinc-500 hover:text-zinc-300"
+			  >
+				{item.name}
+			  </Link>
+			</li>
+		  ))}
+		</ul>
+	  </nav>
+	  <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={10000} />
+	  <h1 className="z-10 text-1xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+		hello [name],
+	  </h1>
+	</div>
+  );
+};
+
+export default Home;
