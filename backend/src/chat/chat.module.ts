640a6e4a9abbe71889d9eda4f81150ed0884b723
nicolasbene <nicolas.benoit.pro@gmail.com>
2024-01-03 23:16:57 +0100

    generate files gateway service controller

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ChatGateway } from './chat.gateway';
+import { ChatService } from './chat.service';
+import { ChatController } from './chat.controller';
 
-@Module({})
+@Module({
+  providers: [ChatGateway, ChatService],
+  controllers: [ChatController]
+})
 export class ChatModule {}
