3aba467e8ff39f56103bb320f58fe8898bf7e155
nicolasbene <nibenoit@student.42.fr>
2024-01-04 18:48:06 +0100

    some stuff on gateway

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,9 +1,28 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+// import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+
+// @WebSocketGateway()
+// export class ChatGateway {
+//   @SubscribeMessage('message')
+//   handleMessage(client: any, payload: any): string {
+//     return 'Hello world!';
+//   }
+// }
+
+import { WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayInit } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnGatewayInit {
+  @WebSocketServer()
+  private server: Server;
+
+  afterInit(server: Server) {
+    // Configure ici tout ce qui doit être fait lors de l'initialisation du serveur WebSocket.
+  }
+
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  handleMessage(client: any, payload: any): void {
+    // Gère ici les messages reçus depuis le client et envoie des mises à jour aux autres clients.
+    this.server.emit('message', payload);
   }
 }
