892734405452d4cfa0b1dac7317d9ffbde15c0d7
Nicolas WYSEUR <nicolaswyseur@gmail.com>
2024-01-05 19:06:46 +0100

    auth ok

--- backend/backend.sh
+++ backend/backend.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 npm install
+npm i --save class-validator class-transformer
+npm add argon2
+
 npx prisma generate
 
 echo "-----Migrating database-----"
 
-npx prisma migrate deploy
+npx prisma db push
 
 echo "-----Starting Prisma Studio on port 5555-----"
 
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,10 @@
+import { ValidationPipe } from '@nestjs/common'; // this is the validation pipe that will be used to validate the data sent to the server
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({whitelist: true})); // this function enables the validation pipe for all routes, validation pipe is used to validate the data sent to the server, whitelist: true means that the validation pipe will only allow the properties that are defined in the DTOs to be sent to the server, if a property is not defined in the DTO, it will be ignored
   app.enableCors();
   await app.listen(8080);
 }
