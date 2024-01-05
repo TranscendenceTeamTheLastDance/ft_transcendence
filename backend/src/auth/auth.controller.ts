2359ee926a4784b6a6b75304b67c148b939095fb
Nicolas WYSEUR <nicolaswyseur@gmail.com>
2024-01-05 19:06:46 +0100

    auth ok

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthDto } from './dto/auth.dto';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
--- backend/src/auth/strategy/jwt.strategy.ts
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -6,27 +6,27 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { DatabaseService } from '../../database/database.service';
 
-@Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy, 'jwt',){
-	constructor(config: ConfigService, private prisma: DatabaseService) {
+@Injectable() //  allow to inject the JwtStrategy class into other classes but also to inject other classes into the JwtStrategy class
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt',) // here we use 'jwt' as the name of the strategy
+{
+	constructor(config: ConfigService, private prisma: DatabaseService)
+	{
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-			secretOrKey: ('secret')
+			secretOrKey: config.get('JWT_SECRET'),
 		});
 	}
 
-	async validate(payload: {
-		sub: string;
-		email: string;
-	}) {
-		const user = 
-		await this.prisma.user.findUnique ({
+	async validate(payload: {sub: number, email: string}) // we use the validate method to extract the payload from the token this function is used by the guard
+	{ 
+		const user = await this.prisma.user.findUnique({
 			where: {
 				id: payload.sub,
-			}, 
+			},
 		});
-		
-		return user; // this is what gets exported to the user controller
+		delete user.hash;
+		return user;
 	}
 	// 401 if user is not found
 }
+
