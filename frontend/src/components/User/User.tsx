fbd30ae3921e5f8f92f338ca036a7771801b1a63
Charlotte MADRANGEAS <charlotte.madrangeas@gmail.com>
2024-01-10 16:23:48 +0100

    some stuff

--- frontend/src/components/User/User.tsx
+++ frontend/src/components/User/User.tsx
@@ -212,12 +212,14 @@ const User = () => {
 			<div className="bg-white rounded-lg z-10">
 				<h2 className="text-black text-xl font-bold mx-auto pt-4 text-center">GAME STATS</h2>
 				<div className="p-4 flex flex-col items-center">
-					<div className="font-bold">Total Games Played</div>
-					<div>{gameStats.totalGamesPlayed}</div>
 					<div className="font-bold mt-4">Wins</div>
 					<CircularProgress winPercentage={winPercentage} />
 					<div className="font-bold mt-4">Losses</div>
 					<CircularProgress winPercentage={lossPercentage} />
+					<div className="font-bold mt-4">Total Games Played</div>
+					<div>{gameStats.totalGamesPlayed}</div>
+					<div className="font-bold mt-4">Ranking</div>
+					<div>{gameStats.Rank} / 1</div>
 				</div>
 			</div>
 		</div>
