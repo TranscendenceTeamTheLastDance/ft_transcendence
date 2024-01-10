fef4079e0e16c0b9b17d3aeaea4d6e5489272bf9
Charlotte MADRANGEAS <charlotte.madrangeas@gmail.com>
2024-01-10 13:28:33 +0100

    makeover: cool frontend look for the user profile

--- frontend/src/components/User/User.tsx
+++ frontend/src/components/User/User.tsx
@@ -1,25 +1,63 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Particles from '../Home/Particles.tsx';
+import ToggleSwitch from './2FAToggle.tsx';
 import avatar_icon from '../assets/avatar.png';
 import axios from 'axios';
 
+const CircularProgress = ({ winPercentage }) => {
+	const radius = 40;
+	const circumference = 2 * Math.PI * radius;
+	const strokePct = ((100 - winPercentage) * circumference) / 100;
+	return (
+	  <svg width={100} height={100}>
+		<circle
+		  stroke="grey"
+		  fill="transparent"
+		  strokeWidth="10"
+		  strokeDasharray={circumference}
+		  style={{ strokeDashoffset: strokePct }}
+		  r={radius}
+		  cx={50}
+		  cy={50}
+		/>
+		<circle
+		  stroke="green"
+		  fill="transparent"
+		  strokeWidth="10"
+		  strokeDasharray={circumference}
+		  style={{ strokeDashoffset: 0 }}
+		  r={radius}
+		  cx={50}
+		  cy={50}
+		/>
+		<text x="50%" y="50%" dominantBaseline="middle" textAnchor="middle">
+		  {winPercentage}%
+		</text>
+	  </svg>
+	);
+  };
 
 const User = () => {
-	const [selectedImage, setSelectedImage] = useState(avatar_icon);
+	const [is2FAEnabled, setIs2FAEnabled] = useState(false);
+	const [selectedImage, setSelectedImage] = useState('');
 	const [userName, setUserName] = useState('');
-	const fileInputRef = useRef(null);
+	const fileInputRef = useRef(avatar_icon);
+	const [winPercentage, setWinPercentage] = useState(0);
 
 	useEffect(() => {
 		const fetchUserData = async () => {
 		  try {
-			const response = await axios.get('http://localhost:8080/users/me', { withCredentials: true });
-			const userData = response.data;
-			setSelectedImage(userData.userPictu); // Set user's picture
-			setUserName(userData.name || 'John Doe'); // Set user's name or default
-		  	} catch (error) {
-			console.error('Error fetching user data:', error);
-			setUserName('John Doe'); // Default name in case of error
-		  }
+				const response = await axios.get('http://localhost:8080/users/me', { withCredentials: true });
+				const userData = response.data;
+				setSelectedImage(userData.userPictu || avatar_icon ); // Set user's picture
+				setUserName(userData.name || 'DEFAULT NAME'); // Set user's name or default
+				if (userData.twoFactorAuthEnabled !== undefined) {
+					setIs2FAEnabled(userData.twoFactorAuthEnabled);
+				  }
+		  	} 
+			catch (error) {
+				console.error('Error fetching user data:', error);
+		  	}
 		};
 	
 		fetchUserData();
@@ -33,7 +71,9 @@ const User = () => {
 	};
 
 	const handleImageClick = () => {
-		fileInputRef.current.click();
+		if (fileInputRef.current) {
+			fileInputRef.current.click();
+		}
 	};
 
 	const handleUpdateProfile = async () => {
@@ -51,36 +91,129 @@ const User = () => {
 		} catch (error) {
 			console.error('Error uploading a new profile pic:', error); }
 		};
+	
+	const handle2FAToggle = () => {
+		setIs2FAEnabled(currentState => !currentState);
+	};
+
+	useEffect(() => {
+		console.log('2FA is now:', is2FAEnabled);
+	  }, [is2FAEnabled]);
+
+
+	const userDetails = {
+		firstName: 'John',
+		lastName: 'Doe',
+		email: 'john.doe@example.com',
+		password: '********',
+	};
+
+	const friends = [
+		{ nickname: 'Friend1', online: true },
+		{ nickname: 'Friend2', online: false },
+		{ nickname: 'Friend3', online: false },
+		{ nickname: 'Friend4', online: false },
+		{ nickname: 'Friend5', online: true },
+		{ nickname: 'Friend6', online: false },
+		{ nickname: 'Friend7', online: true },
+		{ nickname: 'Friend8', online: false },
+		// ... add more friends
+	];
+
+	const gameStats = {
+		totalGamesPlayed: 30,
+		totalWins: 20,
+		totalLosses: 10,
+		Rank: 1,
+		// ... other stats
+	  };
+
+	  useEffect(() => {
+		if (gameStats.totalGamesPlayed > 0) {
+		  const winPct = (gameStats.totalWins / gameStats.totalGamesPlayed) * 100;
+		  setWinPercentage(Math.round(winPct));
+		}
+	  }, [gameStats.totalWins, gameStats.totalGamesPlayed]);
+	
 
 	return (
-		<div className="flex items-center justify-center min-h-screen">
-			<div className="flex flex-col bg-white w-[600px] mx-auto mt-[200px] pb-[30px] rounded-lg">
-				<Particles className="absolute inset-0 -z-10" quantity={1000} />
-				
-				{/* Header */}
-				<h1 className="text-black text-2xl font-bold mx-auto mt-8">USER INFORMATION</h1>
-
-				{/* Avatar and Upload */}
-				<div className="flex items-center mx-auto ml-5 mt-8">
-					<img src={selectedImage} alt="User Avatar" className="w-20 h-20 object-cover mx-auto ml-12 rounded-full cursor-pointer" onClick={handleImageClick} />
-					<input type="file" ref={fileInputRef} onChange={handleImageChange} className="hidden" />
-					<p className="text-lg font-bold ml-8">{userName}</p>
+		<div className="flex flex-col items-center justify-center min-h-screen relative pb-8">
+		  <Particles className="absolute inset-0 -z-10" quantity={1000} />
+	  
+		  {/* User Information Box */}
+		  <div className="bg-white w-[600px] mx-auto mt-[200px] pb-[30px] rounded-lg z-10">
+			<h1 className="text-black text-2xl font-bold mx-auto mt-8 text-center">USER INFORMATION</h1>
+			
+			{/* Profile Section */}
+			<div className="flex mt-8 mx-auto w-3/4">
+			  {/* Left side - Avatar and Nickname */}
+			  <div className="flex flex-col items-center justify-center w-1/4 bg-gray-100 p-4 rounded-lg mr-4">
+				<img src={selectedImage} alt="User Avatar" className="w-20 h-20 object-cover rounded-full cursor-pointer mb-2" onClick={handleImageClick} />
+				<p className="text-center font-bold">{userName}</p>
+				<input type="file" ref={fileInputRef} onChange={handleImageChange} className="hidden" />
+			  </div>
+	  
+			  {/* Right side - User Details */}
+			  <div className="w-3/4 bg-gray-100 p-4 rounded-lg">
+				{Object.entries(userDetails).map(([key, value]) => (
+				  <div key={key} className="flex justify-between mb-2">
+					<span className="font-bold capitalize">{key}:</span>
+					<span>{value}</span>
+				  </div>
+				))}
+				{/* 2FA Toggle */}
+				<div className="flex justify-between">
+				  <span className="font-bold">2FA:</span>
+				  <ToggleSwitch isOn={is2FAEnabled} handleToggle={handle2FAToggle} />
 				</div>
-
-				{/* All the stats go here */}
-				<div className="bg-gray-200 p-4 rounded-lg mx-auto mt-8	w-3/4">
-					<p className="text-center font-bold"> [STATS TO ADD] </p>
-					{/* <p className="text-lg">WINS & LOSSES </p> */}
-				</div>
-
-				{/* Amend button */}
-				<button className="bg-gray-500 text-white py-2 px-4 rounded-full text-lg font-bold my-4 mx-auto"
-					style={{ width: '220px', height: '59px' }}
-					onClick={handleUpdateProfile}
-					>Update Profile</button>
+			  </div>
 			</div>
+	  
+			{/* Update Profile Button */}
+			<div className="flex justify-center mt-4">
+			  <button className="bg-gray-500 text-white py-2 px-4 rounded-lg text-lg font-bold mx-2" onClick={handleUpdateProfile}>
+				Update Profile
+			  </button>
+			</div>
+		  </div>
+	  
+		{/* Space between User Information and Friends Section */}
+		<div className="my-8"></div>
+	  
+		{/* Friends Box */}
+		<div className="bg-white w-[600px] mx-auto pb-[30px] rounded-lg z-10">
+		<h2 className="text-black text-xl font-bold mx-auto pt-4 text-center">FRIENDS</h2>
+		<div className="flex overflow-x-auto py-4 px-4">
+		{friends.map((friend, index) => (
+			<div key={index} className="flex flex-col items-center mr-4">
+			<div className="w-20 h-20 bg-gray-300 rounded-full"></div> {/* Replace with friend's image */}
+			<div className="flex items-center">
+				<p className="text-center font-bold mr-2">{friend.nickname}</p>
+				<span className={`h-3 w-3 rounded-full ${friend.online ? 'bg-green-500' : 'bg-red-500'}`}></span>
+			</div>
+		  </div>
+		))}
 		</div>
-	);
-}
+		</div>
+		
+		{/* Space between Friends and Game Stats Section */}
+		<div className="my-8"></div>
+
+      {/* Game Stats Box */}
+      <div className="bg-white w-[600px] mx-auto pb-[30px] rounded-lg z-10">
+        <h2 className="text-black text-xl font-bold mx-auto pt-4 text-center">GAME STATS</h2>
+        <div className="p-4 flex flex-col items-center">
+          <div className="font-bold">Total Games Played</div>
+          <div>{gameStats.totalGamesPlayed}</div>
+          <div className="mt-4">
+            <CircularProgress winPercentage={winPercentage} />
+          </div>
+          <div className="mt-2">Wins: {gameStats.totalWins} / {gameStats.totalGamesPlayed}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 
 export default User;
