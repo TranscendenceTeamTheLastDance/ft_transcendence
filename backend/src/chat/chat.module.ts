290d212f1050dce2c6806a4796eb607f14e69808
nicolasbene <nibenoit@student.42.fr>
2024-01-19 18:38:23 +0100

    Create Chat Controller, Dtos and Dtos Gateway is OK

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
+import { ChatController } from './chat.controller';
+import { ChannelsService } from './chat.service';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [ChatGateway],
+  controllers: [ChatController],
+  providers: [ChatGateway, ChannelsService],
 })
 export class ChatModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,20 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger as NestLogger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
 
-async function bootstrap() {
+async function bootstrap(): Promise<string>{
+  // Crée une application NestJS
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // Active la validation des DTO
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true,
+    origin: 'http://localhost:3000', // Autorise l'origine de la requête
+    credentials: true, // Autorise l'envoi des cookies
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Ajoute les méthodes HTTP nécessaires
     allowedHeaders: ['Content-Type', 'Authorization'], // Ajoute les en-têtes CORS nécessaires
   };
@@ -23,6 +25,18 @@ async function bootstrap() {
   app.use(cookieParser());
 
   await app.listen(8080);
+  NestLogger.log('API listening on port 8080', 'Server');
+
+  return app.getUrl();
 }
 
-bootstrap();
+// Cette partie de code est une fonction asynchrone auto-exécutée qui appelle la fonction bootstrap() et gère les erreurs éventuelles.
+(async (): Promise<void> => {
+  try {
+    const url = await bootstrap();
+    NestLogger.log(`API up at: ${url}`, 'Server'); // Affiche l'URL de l'API
+  } catch (error) {
+    NestLogger.error(error, 'Server');
+  }
+})();
+
