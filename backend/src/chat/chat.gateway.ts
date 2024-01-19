1e78be7786a4d64b8a2903246a898b7efb96bd66
nicolasbene <nibenoit@student.42.fr>
2024-01-19 14:29:25 +0100

    Refactor WebSocketGateway and ChatPage components

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,19 +1,24 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Server } from 'socket.io';
 
 @WebSocketGateway({
-    cors: {
-        origin : 'http://localhost:3000'//l'origine du message pour autoriser la connection
-    },
-    namespace: 'chat',//specification pour pas que sa rentre en conflit
+  cors: {
+    origin: 'http://localhost:3000', // l'origine du message pour autoriser la connection
+  },
+  namespace: 'chat', // spécification pour éviter les conflits
 })
 export class ChatGateway {
-    @WebSocketServer()
-    server: Server;
+  @WebSocketServer()
+  server: Server;
 
-    @SubscribeMessage('message')
-    handleMessage(@MessageBody() message: string): void {
-        console.log(message);
-        this.server.emit('message', message);
-    }
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: string): void {
+    console.log(message);
+    this.server.emit('message', message);
+  }
 }
--- frontend/src/pages/Messages.tsx
+++ frontend/src/pages/Messages.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 export default function Messages({ messages }: { messages: string[] }) {
-	return (
-		<div >
-			{messages.map((message, index) => (
-				<div style={{ color: 'white' }} key={index}>{message}</div>
-			))}
-		</div>
-	)
+  return (
+    <div>
+      {messages.map((message, index) => (
+        <div style={{ color: "white" }} key={index}>
+          {message}
+        </div>
+      ))}
+    </div>
+  );
 }
