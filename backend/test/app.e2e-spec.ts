ee9bd60304148c6782352dada7e857422041ae9c
Charlotte MADRANGEAS <charlotte.madrangeas@gmail.com>
2024-01-22 14:21:31 +0100

    fetch test environment variables from .env.test file

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -2,9 +2,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import * as dotenv from 'dotenv';
-
-dotenv.config({ path: '../../.env' }); // must add the env variables to the testing env
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
