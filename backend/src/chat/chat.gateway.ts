0f18622551f3239ad1507c9f2c497d6699232222
nicolasbene <nibenoit@student.42.fr>
2024-02-05 18:19:10 +0100

    Petit pas en arriere pour mieux sauter and remove unused code

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,6 +1,7 @@
-import { Logger, OnModuleInit, UseGuards } from '@nestjs/common';
+import { Logger, UseFilters, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import {
+  ConnectedSocket,
   MessageBody,
   OnGatewayInit,
   SubscribeMessage,
@@ -8,42 +9,61 @@ import {
   WebSocketServer,
   WsResponse,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
-import { WSAuthMiddleware } from '../auth/ws/ws.middleware';
+import { BadRequestTransformationFilter } from '../utils/bad-request-exception.filter';
+
+import { ChannelsService } from './channels.service';
+import { CreateChannelDTO, JoinChannelDTO } from './chat.dto';
 import { ChatEvent } from './chat.state';
-import { JwtGuard } from 'src/auth/guard';
 import { UserService } from 'src/user/user.service';
 
-
-@UseGuards(JwtGuard)
+@UsePipes(new ValidationPipe())
 @WebSocketGateway({
-  namespace: 'chat',
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3000', // l'origine du message pour autoriser la connection
   },
+  namespace: 'chat', // spécification pour éviter les conflits
 })
+
+@UseFilters(BadRequestTransformationFilter)
 export class ChatGateway implements OnGatewayInit {
   @WebSocketServer()
   private io: Server;
   private logger: Logger = new Logger(ChatGateway.name);
-  private userService: UserService;
 
-  constructor(private jwtService: JwtService) {}
-
+  constructor(
+    private jwtService: JwtService,
+    private channelsService: ChannelsService,
+    private userService: UserService
+  ) {}
 
   afterInit(server: Server) {
-    const authMiddleware = WSAuthMiddleware(this.jwtService, this.userService);
-    server.use(authMiddleware);
+    this.logger.log('Initialized!');
 
     this.io.on('connection', (socket) => {
       this.logger.log('Client connected: ' + socket.id);
     });
 
-    setInterval(() => this.io.emit('message', 'hello'), 2000);
+    // setInterval(() => this.io.emit('message', 'hello'), 2000);
   }
 
-  @UseGuards(JwtGuard)
+  @SubscribeMessage(ChatEvent.Create)
+  async onCreateChannel(
+    @MessageBody() channel: CreateChannelDTO,
+    @ConnectedSocket() client: Socket,
+  ) {
+    this.logger.log(client.data.user);
+    this.logger.log('Create channel: ' + channel.name);
+    await this.channelsService.createChannel(channel, client.data.user);
+  }
+
+  // @SubscribeMessage(ChatEvent.Join)
+  // async onJoinChannel(@MessageBody() channel: JoinChannelDTO, @ConnectedSocket() client: Socket) {
+  //   await this.channelsService.joinChannel(channel, client.data.user);
+  //   //client.join(channel.channel);
+  // }
+
   @SubscribeMessage(ChatEvent.Message)
   onMessage(@MessageBody() message: string): WsResponse<string> {
     return { event: ChatEvent.Message, data: 'You sent: ' + message };
