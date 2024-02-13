999f2cee986b334dad4f3f0c461146e8001d0cec
nicolasbene <nibenoit@student.42.fr>
2024-02-13 09:56:37 +0100

    Change to relative path

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
 import { ChannelsService } from './channels.service';
 import { PrismaService } from 'nestjs-prisma';
-import { UserService } from 'src/user/user.service';
+import { UserService } from '../user/user.service';
 import { ChatController } from './chat.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from 'src/auth/auth.module';
