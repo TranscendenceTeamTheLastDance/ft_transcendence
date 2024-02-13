08793b38a3ea2224e26b33e1c67f026e1bea3a98
nicolasbene <nibenoit@student.42.fr>
2024-02-13 10:32:36 +0100

    Add Particules to Chat

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Chat from '../components/chat/Chat';
+import Particles from '../components/Home/Particles';
 
 const ChatPage = () => {
   return (
     <div className="flex h-full w-full items-center justify-center gap-2">
+      <Particles className="absolute inset-0 -z-10" quantity={1000} />
       <Chat />
     </div>
   );
