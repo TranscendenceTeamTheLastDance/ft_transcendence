d217df60a353efa45a466b7de1a8f5daec8c9722
Charlotte MADRANGEAS <charlotte.madrangeas@gmail.com>
2024-02-16 10:56:09 +0100

    if no friends display message

--- frontend/src/components/User/UserFriendsList.tsx
+++ frontend/src/components/User/UserFriendsList.tsx
@@ -30,18 +30,22 @@ const FriendsList = () => {
 	return (
 		<div className="bg-white w-[600px] mt-8 pb-[30px] rounded-lg z-10">
 		  <h2 className="text-black text-xl font-bold mx-auto pt-4 text-center">FRIENDS</h2>
-		  <div className="flex justify-center overflow-x-auto py-4 px-4" style={{minHeight: '100px'}}>
-			<div className="flex flex-nowrap" style={{gap: '20px'}}>
-			  {friends.map((friend, index) => (
-				<div key={index} className="flex flex-col items-center" style={{minWidth: '100px'}}>
-				  <img src={friend.profilePic || avatar_icon} alt="User Avatar" style={{width: '70px', height: '70px', borderRadius: '20px'}} />
-				  <p className="text-center font-bold mt-2">{friend.username}</p>
-				</div>
-			  ))}
-			</div>
+		  <div className="flex justify-center items-center overflow-x-auto py-4 px-4" style={{ minHeight: '100px' }}>
+			{friends.length > 0 ? (
+			  <div className="flex flex-nowrap" style={{ gap: '20px' }}>
+				{friends.map((friend, index) => (
+				  <div key={index} className="flex flex-col items-center" style={{ minWidth: '100px' }}>
+					<img src={friend.profilePic || avatar_icon} alt="User Avatar" style={{ width: '70px', height: '70px', borderRadius: '20px' }} />
+					<p className="text-center font-bold mt-2">{friend.username}</p>
+				  </div>
+				))}
+			  </div>
+			) : (
+			  <p className="text-center text-gray-600">Your friends list is looking a little lonely... Start adding friends now!</p>
+			)}
 		  </div>
 		</div>
 	  );
-	}
+}	  
 
 export default FriendsList;
